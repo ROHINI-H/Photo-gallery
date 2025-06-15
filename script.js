6beb01e47b885503771446e9718cbe380d8f75b7
Rohini <h.rohini19@gmail.com>
2025-06-15 22:55:50 +0530

    filter added

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function displayimage(src) {
     document.getElementById("displayArea").style.display = "flex";
-    document.getElementById("showimage").src = src;
+    const img = document.getElementById("showimage")
+    img.src = src;
 }
 
 function closeImage() {
     document.getElementById("displayArea").style.display = "none";
+}
+
+function filterImg(category) {
+    const images = document.querySelectorAll(".container img");
+
+    images.forEach(img => {
+        if (category === "all" || img.classList.contains(category)) {
+            img.style.display = "inline-block";
+        } else {
+            img.style.display = "none";
+        }
+    });
 }
